52263e4b139ce0970eb154d84b1810e392c2b923
Vic Lendrew Cheng <183092900+Chengciv@users.noreply.github.com>
2026-05-19 15:51:01 +0800

    fix: resolve multiple setup and view errors in WS03 project
    - Add missing 'port' key to config/db.php for PDO DSN connection
    - Change username from 'sample' to 'root' and clear password for XAMPP
    - Change require to require_once in public/index.php to prevent duplicate function declaration errors
    - Add \ parameter to loadView() in helpers.php
    - Change <?= ?> to <?php ?> for loadPartials() calls in home.view.php
    - Add PDO::FETCH_OBJ to fetchAll() in home.php to return objects instead of arrays
    - Remove duplicate nested foreach loop in home.view.php

--- Public/index.php
+++ Public/index.php
@@ -1,5 +1,33 @@
-<? 
+<?php
 
-require '../helpers.php';
+require_once '../helpers.php';
+require basePath('Router.php');
+require basePath('Database.php');
 
-loadView('home');
+//Instatiate the router
+$router = new Router();
+
+//Get routes
+$routes = require basePath('routes.php');
+
+//Get current URI and HTTP method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); //Uniform resource identifier
+$method = $_SERVER ['REQUEST_METHOD'];
+
+//Route the request
+$router->route($uri, $method);
+
+//$routes = [
+//    '/' => 'controllers/home.php',
+//    'listings' => 'controllers/listings/index.php',
+//    '/listings/create' => 'controllers/listings/create.php',
+//    '404' => 'controllers/error/404.php'
+//];
+
+//$uri = $_SERVER['REQUEST_URI'];
+//    if(array_key_exists($uri, $routes)) {
+//        require(basePath($routes[$uri]));
+//    } else {
+//        require basePath ($routes['404']);
+//    }
+?>
